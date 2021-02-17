7ce5f13d6279ee228def0f04268054e8d07eb281
Brandon Scott <email@brandonandrewscott.com>
2021-02-16 19:13:37 -0800

    Ideas page getting off to a good start

---
+++ src/js/site/ideaspage-animation.js
@@ -0,0 +1,42 @@
+$(window).on("load", function(){
+
+  if (document.getElementById("ideasPageJSIndicator")) {
+
+    window.setTimeout(function(){$(".loading").addClass("not-loading");}, 100);
+  
+    var tl = gsap.timeline({repeat: 0});
+  
+    tl.to(".loading", {
+      opacity: 0,
+      duration: 1,
+    })
+
+    const latestBlogPostsSectionTL = gsap.timeline( {    
+      scrollTrigger: {
+        trigger: ".latest-blog-posts-section-container",
+        scrub: true,
+        end: "5%",
+        toggleActions: "play reverse play reverse",
+      }
+    });
+  
+    latestBlogPostsSectionTL
+    .to(
+      '.ideaspage-section-transition-container .section-transition.section1', 
+      { 
+        opacity: 0, 
+        duration: 1 
+      },"transition1"
+    );
+    
+    latestBlogPostsSectionTL
+    .to(
+      '.ideaspage-section-transition-container .section-transition.white', 
+      { 
+        opacity: 1, 
+        duration: 1 
+      },"transition1"
+    );
+
+  }
+});
