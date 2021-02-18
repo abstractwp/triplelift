61874152e6450c6ca5643c8ae5db2d8466dbf771
bbaappss <brandonscott@gmail.com>
2021-02-18 00:58:19 -0800

    Ideas page keep goin

--- src/js/site/ideaspage-animation.js
+++ src/js/site/ideaspage-animation.js
@@ -10,34 +10,5 @@ $(window).on("load", function(){
       opacity: 0,
       duration: 1,
     })
-
-    const latestBlogPostsSectionTL = gsap.timeline( {    
-      scrollTrigger: {
-        trigger: ".latest-blog-posts-section-container",
-        scrub: true,
-        start: "0% bottom",
-        end: "20% 95%",
-        toggleActions: "play reverse play reverse",
-      }
-    });
-  
-    latestBlogPostsSectionTL
-    .to(
-      '.ideaspage-section-transition-container .section-transition.section1', 
-      { 
-        opacity: 0, 
-        duration: 1 
-      },"transition1"
-    );
-    
-    latestBlogPostsSectionTL
-    .to(
-      '.ideaspage-section-transition-container .section-transition.white', 
-      { 
-        opacity: 1, 
-        duration: 1 
-      },"transition1"
-    );
-
   }
 });
