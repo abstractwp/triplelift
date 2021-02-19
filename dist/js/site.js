6e6c3ca2b53bb2c3d74ecfdce2bfcf5d1f129702
Brandon Scott <email@brandonandrewscott.com>
2021-02-19 01:57:48 -0800

    Get an animation working for ipad screen

--- dist/js/site.js
+++ dist/js/site.js
@@ -604,6 +604,40 @@ var Lantern = (function() {
 
 })();
 
+$(window).on("load", function(){
+
+  if (document.getElementById("companyPageJSIndicator")) {
+
+    window.setTimeout(function(){$(".loading").addClass("not-loading");}, 100);
+  
+    var tl = gsap.timeline({repeat: 0});
+  
+    tl.to(".loading", {
+      opacity: 0,
+      duration: 1,
+    })
+
+    const missionSectionTransition = gsap.timeline( {    
+      scrollTrigger: {
+        trigger: ".company-page-section.mission",
+        scrub: true,
+        start: "top center",
+        end: "10% center",
+        toggleActions: "play reverse play reverse",
+      }
+    });
+  
+    missionSectionTransition
+    .to(
+      '.company-page-sticky-animation.grey-mark', 
+      { 
+        opacity: 1, 
+        duration: 1 
+      },"transition1"
+    );
+  }
+});
+
 if (document.getElementById("homepageJSIndicator")) {
 gsap.config({nullTargetWarn:false});
 
